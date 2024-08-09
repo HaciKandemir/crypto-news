4945ee8ffa1744922115a1535f2e030b2f39f31e
HaciKandemir <hacikandemir2000@hotmail.com>
2024-08-10 01:11:57 +0300

    feat: add searchByTime endpoint to NewsController

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -27,4 +27,31 @@ class NewsController extends Controller
         return response()->json($news);
     }
 
+    public function searchByTime(Request $request): JsonResponse
+    {
+        $symbol = $request->get('symbol', '');
+        $fromDate = $request->get('fromDate', '');
+        $toDate = $request->get('toDate', '');
+
+        $fromTime = empty($fromDate) ? '-inf' : strtotime($fromDate);
+        $toTime = empty($toDate) ? '+inf' : strtotime($toDate);
+
+        // Get the news IDs for the given symbol and time range
+        if (!empty($symbol)) {
+            $newsIds = Redis::zRevRangeByScore("news:symbol:$symbol", $toTime, $fromTime);
+        } else {
+            $newsIds = Redis::zRevRangeByScore("news:all", $toTime, $fromTime);
+        }
+
+        // Fetch news details using the news IDs
+        $news = array_map(function ($newsId) {
+            $item = Redis::hGetAll("news:$newsId");
+            if (isset($item['symbols'])) {
+                $item['symbols'] = json_decode($item['symbols'], true);
+            }
+            return $item;
+        }, $newsIds);
+
+        return response()->json($news);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/news/search-by-symbol', [NewsController::class, 'searchBySymbol']);
+Route::post('/news/search-by-time', [NewsController::class, 'searchByTime']);
